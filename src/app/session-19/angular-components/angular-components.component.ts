714a7187590b381c476c0b7dc8eedc67dfc1b175
Eas <me@eas.web.id>
2021-10-28 21:58:31 +0700

    add: Session 19 notes

---
+++ src/app/session-19/angular-components/angular-components.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-angular-components',
+  templateUrl: './angular-components.component.html',
+  styleUrls: ['./angular-components.component.css']
+})
+export class AngularComponentsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
