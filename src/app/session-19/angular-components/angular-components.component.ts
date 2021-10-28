e7e8625a44157834072d4058a3aadd7270c1d512
Eas <me@eas.web.id>
2021-10-28 22:29:33 +0700

    update: populate Session 19 notes

--- src/app/session-19/angular-components/angular-components.component.ts
+++ src/app/session-19/angular-components/angular-components.component.ts
@@ -7,9 +7,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AngularComponentsComponent implements OnInit {
 
+  // saya siapkan 2 property dari parent, lalu akan dilempar
+  // setiap value-nya ke ChildComponent
+
+  // Perhatikan panah dari alur passing data!
+
+  //    parentCounter
+  //          |
+  //    parentMessage           name                 passing prop value
+  //          |                  |
+  // Parent ---> ChildComponent ---> ButtonComponent
+  //                    |                   |
+  //                 counter               name      property
+  //                    |
+  //                 message
+  parentCounter = 0
+  parentMessage = 'Hi! I am your parent'
+
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit(): void { }
+
+  // event handlers
+
+  // saya juga siapkan 2 event handler, yang satu untuk
+  // mengubah pesan, yang satunya lagi adalah untuk
+  // menambah counter dengan 1
+
+  // Perhatikan panah dari alur event handler!
+
+  //     lempar event       lempar event
+  // Parent <--- ChildComponent <--- ButtonComponent
+
+  changeMessageOnParent () {
+    this.parentMessage = "Hi! Am I your child?"
+  }
+
+  changeCounterOnParent () {
+    this.parentCounter++
   }
 
 }
--- src/app/session-19/angular-components/child-component/child-component.component.ts
+++ src/app/session-19/angular-components/child-component/child-component.component.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit, Input, Output } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-child-component',
   templateUrl: './child-component.component.html',
   styleUrls: ['./child-component.component.css']
 })
-export class ChildComponentComponent implements OnInit {
+export class ChildComponentComponent {
 
-  constructor() { }
+  // Gunakan ini hanya pada children component!
 
-  ngOnInit(): void {
+  // @Input  --> Terima data yang di-pass dari parent ke child,
+  //             yang "dibuka" sebagai <tag [attribute]="value"></tag>
+  @Input() message = ''
+  @Input() counter = 0
+
+  // @Output --> Lempar event dari child ke parent
+  //             yang "dibuka" sebagai berikut
+  //             <tag (event)="handler()"></tag>
+
+  //             Gunakan EventEmitter berbarengan dengan @Output!
+  @Output() triggerCounter = new EventEmitter()
+  @Output() triggerChangeMessage = new EventEmitter()
+
+  // event handlers
+
+  count () {
+    // emit() dari EventEmitter akan digunakan untuk men-trigger
+    // event dari child component kepada parent component
+    this.triggerCounter.emit()
+  }
+
+  changeMessage () {
+    this.triggerChangeMessage.emit()
   }
 
 }
