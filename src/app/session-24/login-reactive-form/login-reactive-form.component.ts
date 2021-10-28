0e1591e0d8c073e060a11f790c42add0f8f759e7
Eas <me@eas.web.id>
2021-10-28 21:44:50 +0700

    session 24: Reactive Form implementation

--- src/app/session-24/login-reactive-form/login-reactive-form.component.ts
+++ src/app/session-24/login-reactive-form/login-reactive-form.component.ts
@@ -1,26 +1,105 @@
-import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login-reactive-form',
   templateUrl: './login-reactive-form.component.html',
-  styleUrls: ['./login-reactive-form.component.css']
 })
-export class LoginReactiveFormComponent implements OnInit {
+export class LoginReactiveFormComponent {
 
-  constructor() { }
+  // app states
 
-  loginData = new FormGroup({
-    username: new FormControl(''),
-    password: new FormControl('')
-  })
+  loggedIn = false
+  userData: any = {}
+
+  // form states
+
+  // saya kelompokkan form menjadi satu property
+  // yang di dalamnya terdapat semua yang berhubungan
+  // dengan form pada component
+
+  // sangat disarankan untuk kalian kelompokkan
+  // form seperti ini, kecuali jika ada coding guideline
+  // yang sudah ditentukan oleh tim di tempat kalian bekerja
+
+  form: {
+    // penentuan type seperti ini optional,
+    // tapi disarankan agar kalian tidak dihadapi oleh bug
+    // karena type yang berbeda
+    inputData: FormGroup
+    submitted: boolean
+    errors: any
+  } = {
+    // berisi FormGroup, di dalamnya terdapat
+    // kumpulan FormControl.
+    inputData: new FormGroup({
+      // Parameter FormControl dapat kita isi dengan:
+      // - default state value (wajib), dan
+      // - validators (array, optional)
+      username: new FormControl('', [
+        Validators.required,
+        Validators.minLength(5)
+      ]),
+      password: new FormControl('', [
+        Validators.required,
+      ])
+    }),
+    // state yang akan me-monitor form sudah di-submit
+    // oleh user, atau tidak (karena ada perubahan data --
+    // perhatikan event listener di <input> pada
+    // component template!)
+    submitted: false,
+    errors: {}
+  }
+
+  // setters and getters
+
+  get username () {
+    return this.form.inputData.get('username')
+  }
+
+  get password () {
+    return this.form.inputData.get('password')
+  }
+
+  // functions
+
+  validateForm () {
+    if(this.username?.errors)
+      this.form.errors.username = { ...this.username?.errors }
+    else
+      delete this.form.errors.username
+
+    // optional: validasi password
+    // jika diperlukan, akan memeriksa apakah password
+    // sudah terisi atau belum
+    if(this.password?.errors)
+      this.form.errors.password = { ...this.password?.errors }
+    else
+      delete this.form.errors.password
+  }
+
+  // event handlers
 
   handleLoginForm () {
-    console.log(this.loginData.get('username'))
-    console.log(this.loginData.get('password'))
+    this.toggleFormState(true)
+
+    this.validateForm()
+
+    // Jika tidak ada error, maka jalankan perintah ini
+    if(Object.keys(this.form.errors).length === 0) {
+      this.userData.name = this.username?.value
+
+      this.loggedIn = true
+    }
   }
 
-  ngOnInit(): void {
+  toggleFormState (state: boolean) {
+      this.form.submitted = state
   }
 
+  logout () {
+    delete this.userData.name
+    this.loggedIn = false
+  }
 }
