21098f7dd7846288942e0eb9448b184957b4d970
Eas <me@eas.web.id>
2021-10-28 23:01:33 +0700

    update: root App component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  // app states
+
   title = 'angular-notes';
+  currentSession = 19
+  sessions = [
+    19, 20, 21, 22, 23, 24, 25, 26, 27
+  ]
+
+  // form states
+
+  form: {
+    inputData: FormGroup
+  } = {
+    inputData: new FormGroup({
+      session: new FormControl(19)
+    })
+  }
+
+  // setters and getters
+
+  get session () {
+    return this.form.inputData.get('session')
+  }
+
+  // event handler(s)
+
+  changeSession() {
+    if(this.session){
+      const value = this.session.value
+
+      this.currentSession = value
+    }
+  }
 }
