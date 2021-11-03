9f6a98f64596fc2e8997447d4da32ef547389be5
Eas <me@eas.web.id>
2021-11-03 13:30:18 +0700

    populate Session 22 material notes

--- src/app/pages/session22/session22.component.ts
+++ src/app/pages/session22/session22.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Person } from './types/person';
 @Component({
   selector: 'app-session22',
   templateUrl: './session22.component.html',
@@ -9,7 +9,17 @@ export class Session22Component implements OnInit {
 
   constructor() { }
 
+  currentDate: Date = {} as Date;
+
+  person: Person = {} as Person
+
   ngOnInit(): void {
+    this.currentDate = new Date()
+    this.person = {
+      name: "Budi",
+      age: 21,
+      role: "Full-stack developer"
+    };
   }
 
 }
--- src/app/pages/session22/session22.module.ts
+++ src/app/pages/session22/session22.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Session22Component } from './session22.component';
 import { Session22RoutingModule } from './session22-routing.module';
+import { PersonPipe } from './pipes/person.pipe';
 
 
 
 @NgModule({
   declarations: [
-    Session22Component
+    Session22Component,
+    PersonPipe
   ],
   imports: [
     CommonModule,
