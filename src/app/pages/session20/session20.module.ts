f73bdd3924d6504e2ba8400e3b3664450500526f
Eas <me@eas.web.id>
2021-11-03 12:38:52 +0700

    Session 20: Angular Lifecycle + Data Binding recap

--- src/app/pages/session20/session20.module.ts
+++ src/app/pages/session20/session20.module.ts
@@ -2,16 +2,20 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Session20Component } from './session20.component';
 import { Session20RoutingModule } from './session20-routing.module';
+import { AngularDataBindingComponent } from './angular-data-binding/angular-data-binding.component';
+import { FormsModule } from '@angular/forms';
 
 
 
 @NgModule({
   declarations: [
-    Session20Component
+    Session20Component,
+    AngularDataBindingComponent
   ],
   imports: [
     CommonModule,
-    Session20RoutingModule
+    Session20RoutingModule,
+    FormsModule
   ]
 })
 export class Session20Module { }
