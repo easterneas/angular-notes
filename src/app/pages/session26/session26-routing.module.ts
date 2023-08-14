00c935b36d2393e484c00f6f845f8c38d45e4887
Eas <me@eas.web.id>
2023-08-14 20:26:24 +0700

    update: Angular 14

--- src/app/pages/session26/session26-routing.module.ts
+++ src/app/pages/session26/session26-routing.module.ts
@@ -28,12 +28,11 @@ const routes: Routes = [
   }
 ];
 
-// router untuk feature module dapat kalian buat dengan argument...
-//     --routing
-
-// ...ketika kalian membuat module baru dengan
+// router untuk feature module dapat kalian buat dengan argument "--routing"
+// ketika kalian membuat module baru dengan
+//
 //     ng g module <NAMA_MODULE>
-
+//
 // contoh: ng g[enerate] module <NAMA_MODULE> --routing
 
 @NgModule({
